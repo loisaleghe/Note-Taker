183098a2e979de20ec26f25352352a7988a84ded
loisaleghe <loisaleghe@cmail.carleton.ca>
2020-06-09 16:12:09 -0400

    added the GET and POST api routes

--- server.js
+++ server.js
@@ -4,36 +4,48 @@ const path = require("path");
 const PORT = 3000
 const express = require ('express')
 let fs = require('fs')
+const db = require("./db/db.json")
 
 const app = express()
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
-//ROUTES
+//static contemt for the app from the public directory
+app.use(express.static("public"));
 
 //to return the notes.html
 app.get('/notes', function(req,res){
-    res.sendFile(path.join(__dirname + "/Develop/public/notes.html"))
+    res.sendFile(path.join(__dirname + "/public/notes.html"))
 })
 
-//to return the index.html
-app.get(`*`, function(req, res){
-    res.sendFile(path.join(__dirname + "/Develop/public/index.html"))
-})
-
-//read the db.json file
-app.get(`/api/notes`, function(req, res){
-    fs.readFile('db.json', 'utf8', function(error, data){
-        if (error) {
-            return console.log(error)
-          }
-          return data.json()
-    })
+app.get('/api/notes', function(req,res){
+   res.json(db);
 })
 
 //write to db.json file
-app.post(`/api/notes`, function(req, res){
-    fs.writeFile('db.json',)
+app.post('/api/notes', async(req, res) =>{
+    db.push(req.body);
+    
+    console.log(db)
+     fs.writeFile("./db/db.json", JSON.stringify(db), function(error){
+        if (error){
+            return console.log(error)
+        }
+        
+        console.log("success");
+        res.json(db);
+    })
+
+
 })
+
+//to return the index.html
+app.get('*', function(req, res){
+    res.sendFile(path.join(__dirname + "/public/index.html"))
+})
+
+
+
+
 
 app.listen(PORT, () => console.log(`App is listening on PORT ${PORT}`))
