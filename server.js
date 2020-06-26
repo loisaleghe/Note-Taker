e3eee26379f5d2e11fcf7d587307f997a861c19c
Lois Aleghe <loisaleghe@cmail.carleton.ca>
2020-06-26 02:12:34 -0400

    - Changed port number

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //Dependencies
 const http = require("http");
 const path = require("path");
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000
 const express = require("express");
 let fs = require("fs");
 let db = require("./db/db.json");
