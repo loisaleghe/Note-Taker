4a1706486de0229bcc774921ac69b96361abf9a4
Lois Aleghe <loisaleghe@cmail.carleton.ca>
2020-06-26 01:51:53 -0400

    created delete api route and updated code

--- server.js
+++ server.js
@@ -1,51 +1,63 @@
 //Dependencies
-const http = require ('http')
+const http = require("http");
 const path = require("path");
-const PORT = 3000
-const express = require ('express')
-let fs = require('fs')
-const db = require("./db/db.json")
+const PORT = 3000;
+const express = require("express");
+let fs = require("fs");
+let db = require("./db/db.json");
 
-const app = express()
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-//static contemt for the app from the public directory
+//static content for the app from the public directory
 app.use(express.static("public"));
 
 //to return the notes.html
-app.get('/notes', function(req,res){
-    res.sendFile(path.join(__dirname + "/public/notes.html"))
-})
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname + "/public/notes.html"));
+});
 
-app.get('/api/notes', function(req,res){
-   res.json(db);
-})
+app.get("/api/notes", function (req, res) {
+  res.json(db);
+});
 
 //write to db.json file
-app.post('/api/notes', async(req, res) =>{
-    db.push(req.body);
-    
-    console.log(db)
-     fs.writeFile("./db/db.json", JSON.stringify(db), function(error){
-        if (error){
-            return console.log(error)
-        }
-        
-        console.log("success");
-        res.json(db);
-    })
+app.post("/api/notes", async (req, res) => {
+  let newNote = { title: req.body.title, text: req.body.text, id: db.length+1 };
+  db.push(newNote);
+  console.log(db);
+  fs.writeFile("./db/db.json", JSON.stringify(db), function (error) {
+    if (error) {
+      return console.log(error);
+    }
 
+    console.log("success");
+    res.json(db);
+  });
+});
 
-})
+//To delete from db.json file
+app.delete("/api/notes/:id", async (req, res) => {
+  const deleteId = req.params.id;
+  console.log(deleteId);
+  try {
+    if (deleteId) {
+      db = db.filter(function (val) {
+        return val.id != deleteId;
+      });
+    }
+    return res.json(db);
+  } catch {
+    res.status(500).json(err);
+  }
+});
 
 //to return the index.html
-app.get('*', function(req, res){
-    res.sendFile(path.join(__dirname + "/public/index.html"))
-})
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
 
-
-
-
-
-app.listen(PORT, () => console.log(`App is listening on PORT ${PORT}`))
+app.listen(PORT, () =>
+  console.log(`App is listening on: http://localhost:${PORT}`)
+);
